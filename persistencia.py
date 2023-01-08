e4965ad0d757f6f4c68ca947330658910866b3b3
raulvmaster <raul_v_master@outlook.es>
2023-01-08 15:12:12 +0100

    se corrige el guien de persistencia "-" de pytest

--- persistencia.py
+++ persistencia.py
@@ -7,5 +7,6 @@ def guardar_pedido(nombre, apellidos):
     abre un fichero en modo añadir y guarda los parametros, nombre y apellidos
     """
     with open("pedidos.txt", "a", encoding="utf-8") as file:
-        file.write(nombre + " " + apellidos + "\n")
+        file.write("-"+nombre + " " + apellidos + "\n")
         file.close()
+        
